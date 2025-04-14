a4d3a88cb7cbff683b0cc7d204cfda979bac5674
CorneliusMN <cotn@itu.dk>
2025-04-14 10:48:22 +0200

    bug fixing

--- train_demo.py
+++ train_demo.py
@@ -141,7 +141,7 @@ def main():
     if model_name == 'proto':
         print('use proto')
         model = Proto(word_encoder, dot=opt.dot, ignore_index=opt.ignore_index)
-        framework = FewShotNERFramework(train_data_loader, val_data_loader, test_data_loader, use_sampled_data=opt.use_sampled_data, output_file_name = args.output_file_name)
+        framework = FewShotNERFramework(train_data_loader, val_data_loader, test_data_loader, use_sampled_data=opt.use_sampled_data)
     elif model_name == 'nnshot':
         print('use nnshot')
         model = NNShot(word_encoder, dot=opt.dot, ignore_index=opt.ignore_index)
@@ -170,7 +170,7 @@ def main():
         framework.train(model, prefix,
                 load_ckpt=opt.load_ckpt, save_ckpt=ckpt,
                 val_step=opt.val_step, fp16=opt.fp16,
-                train_iter=opt.train_iter, warmup_step=int(opt.train_iter * 0.1), val_iter=opt.val_iter, learning_rate=opt.lr, use_sgd_for_bert=opt.use_sgd_for_bert)
+                train_iter=opt.train_iter, warmup_step=int(opt.train_iter * 0.1), val_iter=opt.val_iter, learning_rate=opt.lr, use_sgd_for_bert=opt.use_sgd_for_bert, output_file_name = args.output_file_name)
     else:
         ckpt = opt.load_ckpt
         if ckpt is None:
